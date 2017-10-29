3f00d84d03c9dac62b73036ae313137e33ee8e0f
gaoqiang xu <xgqpower@163.com>
2017-10-29 09:44:31 +0800

    change urllib3 to requests. fix download failure problem.

--- main.py
+++ main.py
@@ -5,38 +5,41 @@
 __author__ = 'gaoqiangxu'
 
 import os
-# import time
 import sys
 import re
 import mechanicalsoup
 import urllib3
-
-http = urllib3.PoolManager()
+import requests
 
 def real_path(file_name):
     return (os.path.dirname(os.path.realpath(__file__)) + '/' + file_name)
 
 def makeRequest(url):
-    r = http.request('GET', url)
+    r = requests.get(url)
 
-    if r.status == 200:
+    if r.status_code == 200:
         print('request ok:')
-        return r.data
+        return r.text
     else:
-        print("request:" + url + "failed!")
+        print("request:" + url + " failed!")
         return None
 
 def downloadMp3(baseUrl, url):
-    path = baseUrl + '/' + os.path.basename(os.path.normpath(url))
+    print "Start download " + url
+
+    fileName = os.path.basename(os.path.normpath(url))
+    path = baseUrl + '/' + fileName
     print path 
     if (os.path.exists(path)):
+        print '"' + fileName + '"' + " exists, skip."
+        return
+
+    data = makeRequest(url)
+    if (data == None):
+        print fileName + "download failed"
         return
 
     file = open(path, 'wb')
-    data = makeRequest(url)
-    if (data == None):
-        return
-
     file.write(data)
     file.flush()
     file.close()
@@ -46,7 +49,6 @@ def downloadByDetailUrl(baseUrl, browser, url):
     url = "http://dwellingofduels.net" + url
     browser.close()
     browser.open(url)
-    # regex = "*.mp3"
     
     links = browser.links()
 
@@ -62,11 +64,13 @@ def downloadByYear(baseUrl, browser, year=17):
     
     path = "{:}/{:0>2}".format(baseUrl, year)
     if not os.path.exists(path):
-            os.makedirs(path)
+        print "Create folder: " + path
+        os.makedirs(path)
 
     for i in range(1, 12):
         path1 = "{:}/{:0>2}".format(path, i)
         if not os.path.exists(path1):
+            print "Create folder: " + path1
             os.makedirs(path1)
 
         regex = "duels/{:0>2}-{:0>2}-*".format(year, i)
@@ -88,14 +92,14 @@ def main():
 
     dir = real_path("DoD")
     if not os.path.exists(dir):
-            os.makedirs(dir)
+        print "Create folder: " + dir
+        os.makedirs(dir)
 
     url = "http://dwellingofduels.net/duels/"
     browser = mechanicalsoup.StatefulBrowser()
     browser.open(url)
 
     downloadByYear(dir, browser, 17)
-    
     browser.close()
 
 
